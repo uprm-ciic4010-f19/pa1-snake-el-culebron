0520b7d4937f63673218601bf23dc8884ad45cf7
Anthony Sanchez <Anthony Sanchez@DESKTOP-JCDE390>
2019-09-13 16:20:15 -0400

    Score and badApple finish

--- src/Game/Entities/Dynamic/Player.java
+++ src/Game/Entities/Dynamic/Player.java
@@ -21,11 +21,11 @@ public class Player {
 	public boolean justAte;
 	private Handler handler;
 
-	private int score;		//varr score count 
+	private double score;		//varr score count 
 	public double TheScore;	// varr that is displayed on screen
 	public int xCoord;
 	public int yCoord;
-	
+
 
 	public int moveCounter;
 
@@ -68,21 +68,26 @@ public class Player {
 		}
 		if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_UP)){
 			if(direction !="Down")
-			direction="Up";
+				direction="Up";
 		}if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_DOWN)){
 			if(direction!="Up")
-			direction="Down";
+				direction="Down";
 		}if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_LEFT)){
 			if(direction!="Right")
-			direction="Left";
+				direction="Left";
 		}if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_RIGHT)){
 			if(direction!="Left")
-			
-			direction="Right";
+
+				direction="Right";
 
 		}if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_N)) { // Added N key for adding tail
 			justAte = true;
 			handler.getWorld().body.addLast(new Tail(xCoord, yCoord, handler));
+
+		}if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_M)) { // Added N key for adding tail
+			handler.getWorld().appleLocation[xCoord][yCoord] = false;
+			handler.getWorld().playerLocation[handler.getWorld().body.getLast().x][handler.getWorld().body.getLast().y] = false;
+			handler.getWorld().body.removeLast();
 
 		}if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_MINUS)) { //Added - key for slowing down
 			speed -=2;
@@ -95,57 +100,62 @@ public class Player {
 			State.setState(handler.getGame().pauseState);
 		}
 	}
-	
-    public void checkCollisionAndMove(){
-        handler.getWorld().playerLocation[xCoord][yCoord]=false;
-        int x = xCoord;
-        int y = yCoord;
-        switch (direction){
-            case "Left":
-                if(xCoord==0){
-                	xCoord = handler.getWorld().GridWidthHeightPixelCount-1;
-                    kill();
-                }else{
-                    xCoord--;
-                }
-                break;
-            case "Right":
-                if(xCoord==handler.getWorld().GridWidthHeightPixelCount-1){
-                	xCoord = 0;
-                    kill();
-                }else{
-                    xCoord++;
-                }
-                break;
-            case "Up":
-                if(yCoord==0){
-                	yCoord = handler.getWorld().GridWidthHeightPixelCount-1;
-                    kill();
-                }else{
-                    yCoord--;
-                }
-                break;
-            case "Down":
-                if(yCoord==handler.getWorld().GridWidthHeightPixelCount-1){
-                	yCoord = 0;
-                    kill();
-                }else{
-                    yCoord++;
-                }
-                break;
-        }
-        handler.getWorld().playerLocation[xCoord][yCoord]=true;
-        for(int i=0; i<= handler.getWorld().body.size()-1; i++) {
-        	if(xCoord == handler.getWorld().body.get(i).x && yCoord == handler.getWorld().body.get(i).y) {
-        		
-                State.setState(handler.getGame().gameoverState);
 
-        	}
-        }
+	public void checkCollisionAndMove(){
+		handler.getWorld().playerLocation[xCoord][yCoord]=false;
+		int x = xCoord;
+		int y = yCoord;
+		switch (direction){
+		case "Left":
+			if(xCoord==0){
+				xCoord = handler.getWorld().GridWidthHeightPixelCount-1;
+				kill();
+			}else{
+				xCoord--;
+			}
+			break;
+		case "Right":
+			if(xCoord==handler.getWorld().GridWidthHeightPixelCount-1){
+				xCoord = 0;
+				kill();
+			}else{
+				xCoord++;
+			}
+			break;
+		case "Up":
+			if(yCoord==0){
+				yCoord = handler.getWorld().GridWidthHeightPixelCount-1;
+				kill();
+			}else{
+				yCoord--;
+			}
+			break;
+		case "Down":
+			if(yCoord==handler.getWorld().GridWidthHeightPixelCount-1){
+				yCoord = 0;
+				kill();
+			}else{
+				yCoord++;
+			}
+			break;
+		}
+		handler.getWorld().playerLocation[xCoord][yCoord]=true;
+		for(int i=0; i<= handler.getWorld().body.size()-1; i++) {
+			if(xCoord == handler.getWorld().body.get(i).x && yCoord == handler.getWorld().body.get(i).y) {
+
+				State.setState(handler.getGame().gameoverState);
+
+			}
+		}
 
 		if(handler.getWorld().appleLocation[xCoord][yCoord]){
 			Eat();
-			score++;
+			if(Apple.isGood) {
+			score = score + Math.sqrt((2*score)+1);
+			}
+			else {
+				score =score - Math.sqrt(2*score+1);
+			}
 		}
 
 		if(!handler.getWorld().body.isEmpty()) {
@@ -171,11 +181,11 @@ public class Player {
 				}
 				else if (handler.getWorld().appleLocation[i][j]){
 					if( Apple.isGood == false) {
-					g.setColor(Color.BLACK);
-					g.fillRect((i*handler.getWorld().GridPixelsize),
-							(j*handler.getWorld().GridPixelsize),
-							handler.getWorld().GridPixelsize,
-							handler.getWorld().GridPixelsize);
+						g.setColor(Color.BLACK);
+						g.fillRect((i*handler.getWorld().GridPixelsize),
+								(j*handler.getWorld().GridPixelsize),
+								handler.getWorld().GridPixelsize,
+								handler.getWorld().GridPixelsize);
 					}else if (Apple.isGood == true) {
 						g.setColor(Color.RED);
 						g.fillRect((i*handler.getWorld().GridPixelsize),
@@ -301,6 +311,13 @@ public class Player {
 		handler.getWorld().body.addLast(tail);
 		handler.getWorld().playerLocation[tail.x][tail.y] = true;
 	}
+	public void rottenAppleEat() {
+		if (Apple.isGood = false);
+			handler.getWorld().appleLocation[xCoord][yCoord] = false;
+			handler.getWorld().appleOnBoard=false;
+			handler.getWorld().playerLocation[handler.getWorld().body.getLast().x][handler.getWorld().body.getLast().y] = false;
+			handler.getWorld().body.removeLast();
+	}
 
 	public void kill(){
 		lenght = 0;
@@ -310,6 +327,7 @@ public class Player {
 				handler.getWorld().playerLocation[i][j]=false;
 
 			}
+
 		}
 	}
 
@@ -320,10 +338,9 @@ public class Player {
 	public void setJustAte(boolean justAte) {
 		this.justAte = justAte;
 	}
-	public void highScore (Graphics g) //add score to screen** 
-	{   TheScore = Math.sqrt((2*score)+1);
+	public void highScore (Graphics g) { //add score to screen** {  
 	g.setColor(Color.BLACK);
 	g.setFont(new Font ("Times New Roman", 1, 20));
-	g.drawString("Score: "+String.valueOf(TheScore),15,775);
+	g.drawString("Score: "+String.valueOf(score),15,775);
 	}
 }
